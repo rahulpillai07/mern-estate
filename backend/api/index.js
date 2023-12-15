1e48a52f40742d5d4883957a2d3964ca6fceca3d
rahulpillai07 <rahulrajeshpillai@gmail.com>
2023-12-15 13:01:53 +0530

    added a controller and a routes folder for better structuring

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,13 +1,11 @@
 import express from 'express';
 import connect from '../dbConfig/index.js';
-
+import userRouter from '../routes/user.route.js'
 
 const app=express();
 connect();
-
-app.get('/',(req,res)=>{
-    res.send('hello from the / route')
-})
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
 })
+
+app.use('/api/user',userRouter);
