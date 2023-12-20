e7008f3c856321c082cb0117d42ec78c8c43a797
rahulpillai07 <rahulrajeshpillai@gmail.com>
2023-12-20 09:52:57 +0530

    add redux toolkit

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,31 +1,31 @@
-import mongoose from "mongoose";
-import User from "../models/user.model.js";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
+import mongoose from 'mongoose';
+import User from '../models/user.model.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { errorHandler } from '../utils/error.js';
 
 export const signup = async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
+  const { username, email, password } = req.body;
 
-    // Validate input fields (you can customize this based on your requirements)
+  try {
     if (!username || !email || !password) {
-      return res
-        .status(400)
-        .json({ message: "Please provide all required fields", status: 400 });
+      return res.status(400).json({
+        message: 'Please provide all required fields',
+        status: 400,
+      });
     }
 
-    // Check if the user with the provided email already exists
     const checkUser = await User.findOne({
       $or: [{ email: email }, { username: username }],
     });
 
     if (checkUser) {
-      return res
-        .status(400)
-        .json({ message: "User already exists", status: 400 });
+      return res.status(400).json({
+        message: 'User already exists',
+        status: 400,
+      });
     }
 
-    // Hash the password before saving it to the database
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const newUser = new User({
@@ -35,55 +35,60 @@ export const signup = async (req, res) => {
     });
 
     await newUser.save();
-    console.log("User saved successfully");
 
-    return res.status(200).json({
-      message: `User saved successfully`,
+    return res.status(201).json({
+      message: 'User saved successfully',
       data: newUser,
-      status: 200,
+      status: 201,
     });
   } catch (error) {
-    console.error("Error:", error);
-    return res
-      .status(500)
-      .json({ message: "Internal Server Error", status: 500 });
+    console.error('Error:', error);
+    return res.status(500).json(errorHandler(500, 'Internal Server Error'));
   }
 };
 
 export const signin = async (req, res) => {
-  try {
-    const { username, password } = req.body;
+  const { username, password } = req.body;
 
+  try {
     if (!username || !password) {
-      return res
-        .status(400)
-        .json({ message: "Username or Password is missing" });
+      return res.status(400).json({
+        message: 'Username or Password is missing',
+        status: 400,
+      });
     }
 
     const checkUser = await User.findOne({ username });
 
     if (!checkUser) {
-      return res.status(400).json({ message: "Username not found" });
+      return res.status(404).json({
+        message: 'Username not found',
+        status: 404,
+      });
     }
 
     const isPwdValid = await bcrypt.compare(password, checkUser.password);
 
     if (!isPwdValid) {
-      return res.status(401).json({ message: "Incorrect Password" });
+      return res.status(401).json({
+        message: 'Incorrect Password',
+        status: 401,
+      });
     } else {
       const token = jwt.sign({ id: checkUser._id }, process.env.SECRET);
-      const{password:pass,...rest}=checkUser._doc
+      const { password: pass, ...rest } = checkUser._doc;
+
       return res
-        .cookie("acess token ", token, { httpOnly: true })
+        .cookie('access_token', token, { httpOnly: true })
         .status(200)
         .json({
-          message: "sign in succesfull",
-          userDetails:rest,
+          message: 'Sign in successful',
+          userDetails: rest,
           token,
         });
     }
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Internal Server Error" });
+    res.status(500).json(errorHandler(500, 'Internal Server Error'));
   }
 };
--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,86 +1,75 @@
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from '../redux/user/userSlice';
+import axios from 'axios';
 
-const SignIn = () => {
+export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [formData, setFormData] = useState({
-    username: "",
-    password: "",
-  });
-
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
+  const dispatch = useDispatch();
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
   };
-
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-    setLoading(true);
-
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     try {
-      const response = await axios.post(
-        "http://localhost:3000/api/auth/signin",
-        formData
-      );
-
-      if (response.status === 200) {
-        // Successful response
-        navigate("/");
-      } else if (!response.data.success) {
-        setError(response.data.message);
-        setLoading(false);
-        return;
-        // Handle other response statuses
-      }
-    } catch (error) {
-      // Handle errors (e.g., network error, server error)
-      console.error("Error:", error);
+      dispatch(signInStart());
+      const res = await axios.post('http://localhost:3000/api/auth/signin', formData);
+      const data = res.data; 
       
+      console.log(data);
+      if (data.success === false) {
+        dispatch(signInFailure(data.message));
+        return;
+      }
+      dispatch(signInSuccess(data));
+      navigate('/');
+    } catch (error) {
+      dispatch(signInFailure(error.message));
     }
-    setLoading(false);
   };
-
   return (
-    <div className="max-w-lg mx-auto p-4">
-      <h1 className="text-3xl text-center font-semibold my-7">Signin</h1>
-      <form className="flex flex-col gap-5" onSubmit={handleFormSubmit}>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input
-          type="text"
-          placeholder="Username"
-          className="bg-slate-200 border p-3 rounded-lg"
-          id="username"
-          value={formData.username}
-          onChange={handleInputChange}
-          name="username"
+          type='username'
+          placeholder='username'
+          className='border p-3 rounded-lg'
+          id='username'
+          onChange={handleChange}
         />
         <input
-          type="password"
-          placeholder="Password"
-          className="bg-slate-200 border p-3 rounded-lg"
-          id="password"
-          onChange={handleInputChange}
-          value={formData.password}
-          name="password"
+          type='password'
+          placeholder='password'
+          className='border p-3 rounded-lg'
+          id='password'
+          onChange={handleChange}
         />
-        <button disabled={loading}className="p-3 rounded-lg text-white hover:opacity-90 bg-blue-500">
-          {loading ? "Loading..." : "SIGN IN"}
+
+        <button
+          disabled={loading}
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+        >
+          {loading ? 'Loading...' : 'Sign In'}
         </button>
       </form>
-      {error && <div style={{ color: "red", marginTop: "10px" }}>{error}</div>}
-      <div className="flex mt-5">
-        <p className="mr-2">Dont have an account?</p>
-        <Link to={"/sign-up"}>
-          <span className="text-blue-800 ">Sign Up</span>
+      <div className='flex gap-2 mt-5'>
+        <p>Dont have an account?</p>
+        <Link to={'/sign-up'}>
+          <span className='text-blue-700'>Sign up</span>
         </Link>
       </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
   );
-};
-
-export default SignIn;
+}
